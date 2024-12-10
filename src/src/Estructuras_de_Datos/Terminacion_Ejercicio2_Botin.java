2cae3a434d763b5f2525d406da6be21d016396b8
PauMoreno <paumorenocatalan@gmail.com>
2024-12-10 20:42:06 +0100

    10/12/35

--- src/src/Estructuras_de_Datos/Terminacion_Ejercicio2_Botin.java
+++ src/src/Estructuras_de_Datos/Terminacion_Ejercicio2_Botin.java
@@ -7,44 +7,30 @@ import java.util.Scanner;
 public class Terminacion_Ejercicio2_Botin {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         String respuesta = "";
-        Map<String, ArrayList> mapa1 = new HashMap<String, ArrayList>();
-        ArrayList<String> numeros = new ArrayList<>();
-        numeros.add("655056836");
-        numeros.add("566056836");
-        mapa1.put("PauMo", numeros);
+        int numero = 0;
+        int numTripulacion;
 
-        numeros = new ArrayList<>();
-        numeros.add("693790634");
-        mapa1.put("Foga", numeros);
+        ArrayList<Integer>billetes = new ArrayList<>();
+        Map<Integer, Integer> reparto = new HashMap<>();
 
-        numeros = new ArrayList<>();
-        numeros.add("644280983");
-        mapa1.put("AlexCraft", numeros );
 
-        numeros = new ArrayList<>();
-        numeros.add("642832231");
-        mapa1.put("Leonidas", numeros );
+        System.out.println("Introduce todos los billetes conseguidos de uno a uno en la bolsa");
+        while (true){
+            System.out.println("Introduce un billete (Para finalizar presiona enter en blanco)");
+            respuesta = scanner.nextLine();
+            if (respuesta.isBlank()) break;
+            numero = Integer.parseInt(respuesta);
+            billetes.add(numero);
+        }
 
-        numeros = new ArrayList<>();
-        numeros.add("633844956");
-        mapa1.put("Vicas", numeros );
+        System.out.println("-----------------------------------------------------------------");
+        System.out.println("Cuantos miembros sois en la tripulacion");
+        numTripulacion = scanner.nextInt();
 
-        numeros = new ArrayList<>();
-        numeros.add("608293195");
-        mapa1.put("Xusmuelas", numeros );
+//        for (int billete : billetes) {
+//            reparto.put()
+//        }
 
-        System.out.println(mapa1.keySet());
-        System.out.println("-----------------------------");
-        System.out.println("Introduce el nombre del contacto");
-        respuesta = scanner.nextLine();
-        capital(mapa1, respuesta);
-    }
-
-    public static String capital (Map<String, ArrayList> mapa1, String respuesta){
-        if (mapa1.containsKey(respuesta)) System.out.println("Su telefono: " + mapa1.get(respuesta));
-        else System.out.println("Valor no valido");
-        return respuesta;
     }
 }
---
+++ src/src/Estructuras_de_Datos/Terminacion_Ejercicio3_Frequency.java
@@ -0,0 +1,29 @@
+package Estructuras_de_Datos;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Terminacion_Ejercicio3_Frequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> mapa1 = new HashMap<>();
+        ArrayList<String> palabras = new ArrayList<>();
+        String respuesta = "";
+
+        while (true){
+            System.out.println("Introduce una palabra (Para finalizar presione enter sin nada escrito en la terminal)");
+            respuesta = scanner.nextLine();
+
+            if (respuesta.isBlank()) break;
+            palabras.add(respuesta);
+        }
+        scanner.close();
+
+        for (String palabra : palabras){
+            mapa1.put(palabra.toLowerCase(), mapa1.getOrDefault(palabra,0) +1);
+        }
+
+        System.out.println(mapa1);
+    }
+}
