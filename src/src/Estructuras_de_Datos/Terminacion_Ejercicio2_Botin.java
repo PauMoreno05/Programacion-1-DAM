322d92bc1feb1e6dc769426c4545fc0556da89a6
PauMoreno <paumorenocatalan@gmail.com>
2024-12-11 20:41:28 +0100

    10/12/35

--- src/src/Estructuras_de_Datos/Terminacion_Ejercicio2_Botin.java
+++ src/src/Estructuras_de_Datos/Terminacion_Ejercicio2_Botin.java
@@ -10,9 +10,10 @@ public class Terminacion_Ejercicio2_Botin {
         String respuesta = "";
         int numero = 0;
         int numTripulacion;
+        int miembroActual = 0;
 
         ArrayList<Integer>billetes = new ArrayList<>();
-        Map<Integer, Integer> reparto = new HashMap<>();
+        Map<Integer, ArrayList<Integer>>reparto = new HashMap<>();
 
 
         System.out.println("Introduce todos los billetes conseguidos de uno a uno en la bolsa");
@@ -28,9 +29,20 @@ public class Terminacion_Ejercicio2_Botin {
         System.out.println("Cuantos miembros sois en la tripulacion");
         numTripulacion = scanner.nextInt();
 
-//        for (int billete : billetes) {
-//            reparto.put()
-//        }
+        for (int i = 0; i < numTripulacion; i++){
+            reparto.put(i,new ArrayList<>());
+        }
+
+        for (int billete : billetes) {
+            reparto.get(miembroActual).add(billete);
+            miembroActual++;
+            if (miembroActual == numTripulacion){
+                miembroActual = 0;
+            }
+        }
+
+        System.out.println("El resultado del reparto del botin es el siguiente:");
+        System.out.println(reparto);
 
     }
 }
