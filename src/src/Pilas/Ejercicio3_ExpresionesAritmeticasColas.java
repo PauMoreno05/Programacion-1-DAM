e9a5dfbedec3e5711af827b8f8633466ebdf1542
PauMoreno <paumorenocatalan@gmail.com>
2025-01-13 20:42:14 +0100

    Pilas y colas

---
+++ src/src/Pilas/Ejercicio3_ExpresionesAritmeticasColas.java
@@ -0,0 +1,40 @@
+package Pilas;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Ejercicio3_ExpresionesAritmeticasColas {
+    public static void main(String[] args) {
+        Queue<Integer> cola = new PriorityQueue<>();
+        String operacion = "3 5 4 + *";
+        String [] operacionFin;
+        operacionFin = operacion.split(" ");
+
+
+        for (String i : operacionFin){
+            if (Character.isDigit(i.charAt(0))){
+                cola.offer(Integer.parseInt(i));
+            }else{
+                int n1 = cola.poll();
+                int n2 = cola.poll();
+                switch (i){
+                    case "+" :
+                        cola.offer(n1 + n2);
+                        break;
+                    case "-" :
+                        cola.offer(n1 - n2);
+                        break;
+                    case "*" :
+                        cola.offer(n1 * n2);
+                        break;
+                    case "/" :
+                        cola.offer(n1 / n2);
+                        break;
+
+                    default: break;
+                }
+            }
+        }
+        System.out.println(cola.poll());
+    }
+}
