1f144bac1a2443d340a1e36886bc510ad00e1d45
pauLinux <paumorenocatalan@gmail.com>
2025-02-06 20:37:08 +0100

    Comienzo de MaquinaExpendedora

---
+++ src/src/POO/maquinaExpendedora/MainMaquina.java
@@ -0,0 +1,13 @@
+package POO.maquinaExpendedora;
+
+public class MainMaquina {
+    public static void main(String[] args) {
+
+        System.out.println("------------------------------");
+        System.out.println("Maquina Expendedora de Tickets");
+        System.out.println("------------------------------");
+        System.out.println("Que desea hacer (Sacar ticket / Pagar ticket)");
+
+
+    }
+}
---
+++ src/src/POO/maquinaExpendedora/MaquinaExpendedora.java
@@ -0,0 +1,21 @@
+package POO.maquinaExpendedora;
+
+public class MaquinaExpendedora {
+    private double caja;
+
+    public MaquinaExpendedora(double caja){
+        this.caja = caja;
+    }
+
+    public double getCaja() {
+        return caja;
+    }
+
+    public void setCaja(double caja) {
+        this.caja = caja;
+    }
+    public void sacarDinero(double dinero){
+        double[] monedas = {2, 1, 0.50,0.20,0.10,0.05};
+    }
+
+}
